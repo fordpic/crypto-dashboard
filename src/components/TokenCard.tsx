69d7b62694e3c1328077d69fbc1d65463be21818
Ford Pickert <fordpickert@gmail.com>
2023-03-23 15:11:42 -0500

    fetch working, price feed rendering

--- src/components/TokenCard.tsx
+++ src/components/TokenCard.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import { parseNum } from '@/utils';
 
-export default function TokenCard() {
-	return <div>TokenCard</div>;
+export default function TokenCard({ token }: any) {
+	const router = useRouter();
+
+	return (
+		<>
+			<div
+				onClick={() => router.push(`/tokens/${token.id}`)}
+				className='flex items-center mx-24 text-center justify-center border-b border-blue-400/75 hover:bg-slate-700'>
+				{/* Image */}
+				<div>
+					<img src={token.image} alt='logo' className='h-12' />
+				</div>
+
+				{/* Name + Symbol */}
+				<div className='text-center items-center justify-center w-[30%]'>
+					{token?.name} - {token?.symbol.toUpperCase()}
+				</div>
+
+				{/* Current Price */}
+				<div className='w-[30%]'>
+					Current Price: ${parseNum(token.current_price)}
+				</div>
+
+				{/* High + Low + % change */}
+				<div className='flex-col w-3/12'>
+					High (24h): ${parseNum(token?.high_24h)}
+					<br />
+					Low (24h): ${parseNum(token?.low_24h)}
+					<br />
+					{token?.price_change_percentage_24h > 0 ? (
+						<span className='text-green-400'>
+							{token?.price_change_percentage_24h.toFixed(2)}%
+						</span>
+					) : (
+						<span className='text-red-500'>
+							{token?.price_change_percentage_24h.toFixed(2)}%
+						</span>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
--- src/pages/PriceFeed.tsx
+++ src/pages/PriceFeed.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
+import useSWR from 'swr';
 import Layout from '@/components/Layout';
 import Footer from '@/components/Footer';
 import SearchBar from '@/components/SearchBar';
+import { fullCoinAPI, fetcher } from '@/utils';
+import TokenCard from '@/components/TokenCard';
 
 export default function PriceFeed() {
+	const { data } = useSWR(fullCoinAPI, fetcher);
+
 	return (
 		<>
 			<Head>
@@ -19,7 +24,21 @@ export default function PriceFeed() {
 			<SearchBar />
 			<main>
 				<Layout>
-					<div>Prices go here</div>
+					<h2 className='text-center text-4xl text-blue-500 font-bold tracking-wide'>
+						PRICE FEED
+					</h2>
+
+					<h4 className='text-center text-xl font-semibold pt-4'>
+						Top 100 Tokens By Market Cap
+					</h4>
+
+					<div className='border-2 border-pink-400 mt-4 justify-center items-center text-center'>
+						<div>
+							{data?.map((token: any) => (
+								<TokenCard key={token.id} token={token} />
+							))}
+						</div>
+					</div>
 				</Layout>
 			</main>
 			<Footer />
