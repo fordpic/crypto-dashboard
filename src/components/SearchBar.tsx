956f4324432669d3851e82c59af6630f50f9e860
Ford Pickert <fordpickert@gmail.com>
2023-03-23 19:15:18 -0500

    mvp complete

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,18 @@
+// On ice for now
+
+import { useRouter } from 'next/router';
+
 export default function SearchBar() {
+	const router = useRouter();
+	const searchInput = document.getElementById('search-input');
+
 	return (
 		<div className='rounded-xl p-2 ml-auto flex'>
 			<input
+				id='search-input'
 				className='border-2 border-blue-500 rounded-xl'
-				placeholder='Search'></input>
+				placeholder='Search'
+				type='search'></input>
 		</div>
 	);
 }
