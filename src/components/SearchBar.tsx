355a448ce863265a080cdf6467e9231307766d12
Ford Pickert <fordpickert@gmail.com>
2023-04-01 15:35:13 -0500

    put search bar on ice for now

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,39 +3,39 @@ import { useRouter } from 'next/router';
 
 // On ice for now
 
-export default function SearchBar({ data }: any) {
-	const router = useRouter();
-	const [query, setQuery] = useState('');
-	const id = data?.id;
-	const name = data?.name;
+// export default function SearchBar({ data }: any) {
+// 	const router = useRouter();
+// 	const [query, setQuery] = useState('');
+// 	const id = data?.id;
+// 	const name = data?.name;
 
-	const filteredTokens = data?.filter((req: string) => {
-		if (query) {
-			if (
-				id?.toLowerCase().includes(query.toLowerCase()) ||
-				name?.toLowerCase().includes(query.toLowerCase())
-			) {
-				router.push(`tokens/${query}`);
-			} else {
-				router.push(`/Error`);
-			}
-		}
-	});
+// 	const filteredTokens = data?.filter((req: string) => {
+// 		if (query) {
+// 			if (
+// 				id?.toLowerCase().includes(query.toLowerCase()) ||
+// 				name?.toLowerCase().includes(query.toLowerCase())
+// 			) {
+// 				router.push(`tokens/${query}`);
+// 			} else {
+// 				router.push(`/Error`);
+// 			}
+// 		}
+// 	});
 
-	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setQuery(event.target.value);
-		filteredTokens(query);
-		console.log(query);
-	};
+// 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+// 		setQuery(event.target.value);
+// 		filteredTokens(query);
+// 		console.log(query);
+// 	};
 
-	return (
-		<div className='rounded-xl p-2 ml-auto flex'>
-			<input
-				className='border-2 border-blue-500 rounded-xl'
-				placeholder='Search'
-				onChange={handleInputChange}
-				value={query}
-				type='search'></input>
-		</div>
-	);
-}
+// 	return (
+// 		<div className='rounded-xl p-2 ml-auto flex'>
+// 			<input
+// 				className='border-2 border-blue-500 rounded-xl'
+// 				placeholder='Search'
+// 				onChange={handleInputChange}
+// 				value={query}
+// 				type='search'></input>
+// 		</div>
+// 	);
+// }
